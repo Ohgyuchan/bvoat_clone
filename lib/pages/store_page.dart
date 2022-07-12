ebd0221cf704759561e41b42953b32f3fd37976d
Ohgyuchan <gyu3637@gmail.com>
2022-07-12 16:07:37 +0900

    home indicator & countup

--- lib/pages/store_page.dart
+++ lib/pages/store_page.dart
@@ -1,6 +1,7 @@
 import 'package:bvoat_clone/pages/data/product_model.dart';
 import 'package:bvoat_clone/pages/data/product_repository.dart';
 import 'package:bvoat_clone/theme/app_color.dart';
+import 'package:countup/countup.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,90 +9,166 @@ import 'data/data_utils.dart';
 import 'tab_enum.dart';
 
 class StorePage extends StatelessWidget {
-  const StorePage({Key? key}) : super(key: key);
+  StorePage({Key? key}) : super(key: key);
+  final _productController = Get.put(ProductController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bvoat'),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.search,
-          ),
-          onPressed: () {},
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
+        appBar: AppBar(
+          title: const Text('Bvoat'),
+          leading: IconButton(
             icon: const Icon(
-              Icons.shopping_cart_outlined,
+              Icons.search,
+            ),
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.shopping_cart_outlined,
+              ),
+            ),
+          ],
+        ),
+        body: DefaultTabController(
+          initialIndex: 1,
+          length: TabEnum.values.length,
+          child: Obx(
+            () => SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TabBar(
+                    isScrollable: true,
+                    tabs: List.generate(
+                      TabEnum.values.length,
+                      (index) => Text(TabEnum.values.elementAt(index).name),
+                    ),
+                  ),
+                  adCard(),
+                  trackingCard(),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
+                    child: Column(
+                      children: [
+                        tshirtsCards(
+                          '옷잘러들의 친환경🌱 그래PICK!',
+                          '여름 흰티 + 그래픽은 치트키니까요 🤫',
+                          ProductsRepository.loadProducts()
+                              .getRange(0, 2)
+                              .toList(),
+                          _productController.scrollController0.value,
+                          _productController.offset0.value,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
+                    child: Column(
+                      children: [
+                        tshirtsCards(
+                          '🧐 이 브랜드, 이 상품이 친환경?',
+                          '이제 브랜드도 친환경이 대세입니다.',
+                          ProductsRepository.loadProducts()
+                              .getRange(2, 4)
+                              .toList(),
+                          _productController.scrollController1.value,
+                          _productController.offset1.value,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
+                    child: Column(
+                      children: [
+                        tshirtsCards(
+                          '상상도 못한 소재 (Feat.환경을 위하여 👍)',
+                          '신기한 소재로 업사이클링 된 상품만 모았어요',
+                          ProductsRepository.loadProducts()
+                              .getRange(4, 6)
+                              .toList(),
+                          _productController.scrollController2.value,
+                          _productController.offset2.value,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
+                    child: Column(
+                      children: [
+                        tshirtsCards(
+                          '새 신을 신고 뛰어보자 팔짝! 👟',
+                          '이번 여름은 환경과 함께하는 스니커즈 어때요?',
+                          ProductsRepository.loadProducts()
+                              .getRange(9, 11)
+                              .toList(),
+                          _productController.scrollController4.value,
+                          _productController.offset4.value,
+                        ),
+                      ],
+                    ),
+                  ),
+                  terms(),
+                ],
+              ),
             ),
           ),
-        ],
-      ),
-      body: DefaultTabController(
-        initialIndex: 1,
-        length: TabEnum.values.length,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TabBar(
-                isScrollable: true,
-                tabs: List.generate(
-                  TabEnum.values.length,
-                  (index) => Text(TabEnum.values.elementAt(index).name),
-                ),
-              ),
-              adCard(),
-              trackingCard(),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    tshirtsCards(),
-                  ],
-                ),
-              ),
-              terms(),
-            ],
-          ),
-        ),
-      ),
-    );
+        ));
   }
 
-  Widget tshirtsCards() => Column(
+  Widget tshirtsCards(
+          String title,
+          String subTitle,
+          List<ProductModel> productModels,
+          ScrollController scrollController,
+          double offset) =>
+      Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text(
-            '옷잘러들의 친환경🌱 그래PICK!',
-            style: TextStyle(
-              fontSize: 25,
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 21,
               fontWeight: FontWeight.bold,
             ),
           ),
-          const Text(
-            '여름 흰티 + 그래픽은 치트키니까요 🤫',
-            style: TextStyle(
+          Text(
+            subTitle,
+            style: const TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.w200,
             ),
           ),
           SizedBox(
-            height: 500,
-            child: GridView.builder(
-              scrollDirection: Axis.horizontal,
-              shrinkWrap: false,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisExtent: 150,
-              ),
-              itemCount: 10,
-              itemBuilder: ((context, index) => productCard(
-                  ProductsRepository.loadProducts().elementAt(index % 2))),
-            ),
+              height: 500,
+              child: GridView.builder(
+                controller: scrollController,
+                physics: const ClampingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisExtent: 160,
+                ),
+                itemCount: 10,
+                itemBuilder: ((context, index) {
+                  return productCard(
+                    productModels.elementAt(index % 2),
+                  );
+                }),
+              )),
+          LinearProgressIndicator(
+            value: offset,
+            color: AppColor.instance.kBlue,
           ),
         ],
       );
@@ -157,8 +234,19 @@ class StorePage extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                  Countup(
+                    begin: 0,
+                    end: 9231,
+                    separator: ',',
+                    duration: const Duration(seconds: 1),
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: AppColor.instance.kBlue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   Text(
-                    '9231명',
+                    '명',
                     style: TextStyle(
                       fontSize: 25,
                       color: AppColor.instance.kBlue,
@@ -181,18 +269,21 @@ class StorePage extends StatelessWidget {
 
   Widget adCard() => Column(
         children: [
-          Container(
+          SizedBox(
             height: Get.height / 2.5,
             width: Get.width,
-            color: Colors.grey,
+            child: Image.asset(
+              'assets/ad.png',
+              fit: BoxFit.fill,
+            ),
           ),
           Container(
             margin: const EdgeInsets.symmetric(vertical: 6),
             height: 10,
-            width: 25,
+            width: 20,
             decoration: BoxDecoration(
                 color: AppColor.instance.kBlue,
-                borderRadius: BorderRadius.circular(15)),
+                borderRadius: BorderRadius.circular(25)),
           ),
         ],
       );
@@ -325,4 +416,62 @@ class StorePage extends StatelessWidget {
           ),
         ],
       );
+}
+
+class ProductController extends GetxController {
+  var scrollController0 = ScrollController(initialScrollOffset: 0.0).obs;
+  var scrollController1 = ScrollController(initialScrollOffset: 0.0).obs;
+  var scrollController2 = ScrollController(initialScrollOffset: 0.0).obs;
+  var scrollController3 = ScrollController(initialScrollOffset: 0.0).obs;
+  var scrollController4 = ScrollController(initialScrollOffset: 0.0).obs;
+
+  var offset0 = 0.0.obs;
+  var offset1 = 0.0.obs;
+  var offset2 = 0.0.obs;
+  var offset3 = 0.0.obs;
+  var offset4 = 0.0.obs;
+
+  @override
+  void onInit() {
+    scrollController0.value.addListener(() {
+      var maxOffset = scrollController0.value.position.maxScrollExtent;
+      var currentOffset = scrollController0.value.offset;
+      var offsetRatio = currentOffset / maxOffset;
+
+      // if (offsetRatio < 0.3) {
+      //   offsetRatio = 0.3;
+      // } else if (offsetRatio < 0.6) {
+      //   offsetRatio = 0.6;
+      // } else {
+      //   offsetRatio = 1;
+      // }
+
+      offset0.value = offsetRatio;
+    });
+    scrollController1.value.addListener(() {
+      var maxOffset = scrollController1.value.position.maxScrollExtent;
+      var currentOffset = scrollController1.value.offset;
+      var offsetRatio = currentOffset / maxOffset;
+      offset1.value = offsetRatio;
+    });
+    scrollController2.value.addListener(() {
+      var maxOffset = scrollController2.value.position.maxScrollExtent;
+      var currentOffset = scrollController2.value.offset;
+      var offsetRatio = currentOffset / maxOffset;
+      offset2.value = offsetRatio;
+    });
+    scrollController3.value.addListener(() {
+      var maxOffset = scrollController3.value.position.maxScrollExtent;
+      var currentOffset = scrollController3.value.offset;
+      var offsetRatio = currentOffset / maxOffset;
+      offset3.value = offsetRatio;
+    });
+    scrollController4.value.addListener(() {
+      var maxOffset = scrollController4.value.position.maxScrollExtent;
+      var currentOffset = scrollController4.value.offset;
+      var offsetRatio = currentOffset / maxOffset;
+      offset4.value = offsetRatio;
+    });
+    super.onInit();
+  }
 }
